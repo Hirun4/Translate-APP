138f12fd75ee46b7d082c0f996387ee035cdcfc0
MSI <hirun08072002@gmail.com>
2024-02-06 10:23:21 +0530

    setup

---
+++ lib/language_translation.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LanguageTranslationPage extends StatefulWidget {
+  const LanguageTranslationPage({super.key});
+
+  @override
+  State<LanguageTranslationPage> createState() =>
+      _LanguageTranslationPageState();
+}
+
+class _LanguageTranslationPageState extends State<LanguageTranslationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" Language Translator "),
+        centerTitle: true,
+      ),
+    );
+  }
+}
